3b6f6ae22cefa0da4c8f15f231def40f23aa44e4
InsidE <insiiide@gmail.com>
2016-06-11 18:55:01 +0400

    Add UsersInfo class

---
+++ instagram/src/results/inc/UsersInfo.h
@@ -0,0 +1,35 @@
+#ifndef USERS_INFO_H
+#define USERS_INFO_H
+
+#include "BaseResult.h"
+#include "UserInfo.h"
+
+#include <vector>
+
+namespace Instagram{
+
+class UsersInfo : public BaseResult{
+public:
+    UsersInfo();
+    UsersInfo(const UsersInfo& info);
+    UsersInfo(UsersInfo&& info);
+
+    UsersInfo(const char* err_msg);
+    UsersInfo(const std::string& err_msg);
+
+    ~UsersInfo();
+
+    UsersInfo& operator=(const UsersInfo& info);
+    UsersInfo& operator=(UsersInfo&& info);
+    
+    UsersInfo& operator<<(const UserInfo& info);
+
+    void add_info(const UserInfo& info);
+
+    const std::vector<UserInfo>& get_users() const;
+private:
+    std::vector<UserInfo> users;
+};
+
+}
+#endif
---
+++ instagram/src/results/src/UsersInfo.cpp
@@ -0,0 +1,48 @@
+#include "UsersInfo.h"
+
+namespace Instagram{
+
+UsersInfo::UsersInfo() : BaseResult{}, users(0) {}
+
+UsersInfo::UsersInfo(const UsersInfo& info) : BaseResult{info}, users{info.users} {}
+
+UsersInfo::UsersInfo(UsersInfo&& info) : BaseResult{std::forward<BaseResult>(info)}, users{std::move(info.users)} {}
+
+UsersInfo::~UsersInfo() {}
+
+UsersInfo& UsersInfo::operator=(const UsersInfo& info){
+    if(this == &info){
+        return *this;
+    }
+    
+    BaseResult::operator=(info);
+    users = info.users;
+
+    return *this;
+}
+
+UsersInfo& UsersInfo::operator=(UsersInfo&& info){
+    if(this == &info){
+        return *this;
+    }
+
+    BaseResult::operator=(std::forward<BaseResult>(info));
+    users = std::move(info.users);
+
+    return *this;
+}
+
+UsersInfo& UsersInfo::operator<<(const UserInfo& info){
+    users.push_back(info);
+    return *this;
+}
+
+void UsersInfo::add_info(const UserInfo& info){
+    users.push_back(info);
+}
+
+const std::vector<UserInfo>& UsersInfo::get_users() const{
+    return users;
+}
+
+}
