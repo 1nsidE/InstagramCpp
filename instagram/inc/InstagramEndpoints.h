986657f09f836d0323e368c17ab613df64b056f8
InsidE <insiiide@gmail.com>
2016-07-16 17:36:43 +0400

    Moved constans from InstagramEndpoints header to InstagramConstants header

--- instagram/inc/InstagramEndpoints.h
+++ instagram/inc/InstagramEndpoints.h
@@ -6,17 +6,6 @@
 #define HTTPSERVER_INSTAGRAMMETHODS_H
 
 namespace Instagram{
-    static const char* GET_AUTH_CODE = "/oauth/access_token";
-    static const char* SELF = "self";
-    static const char* QUERY_ARG = "q";   
-    static const char* LAT_ARG = "lat";
-    static const char* LNG_ARG = "lng";
-    static const char* DST_ARG = "distance";
-    static const char* COUNT_ARG = "count";
-    static const char* MIN_ID_ARG = "min_id";
-    static const char* MAX_ID_ARG = "max_id";
-    static const char* MAX_LIKE_ID = "max_like_id";
-
     namespace Users{
         static const char* users = "/v1/users/";
         static const char* recent_media = "/media/recent";
